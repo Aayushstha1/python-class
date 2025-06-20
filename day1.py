7010a33c43a2eec71536b762b81da9ebc7decb4d
Aayush stha <sudipk339@gmail.com>
2025-06-20 13:54:57 +0545

    Class

---
+++ day1.py
@@ -0,0 +1,37 @@
+# print("Hello, World!")
+# a= 12
+# b= str(a)
+# print(type(b))
+
+# a ="Aayush"
+# print(a[-3:])  
+
+# a= "Aayush"
+# b= "stha"
+# print("MY name is " + a + " " + b)
+# print(f"My name is {a} {b}")  
+
+# a=12
+# b=13
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)  
+# print(a%b)
+# print(a**b) 
+
+#PMDAS
+
+
+# a=  int (input ("Input here"))
+# print(type(a))
+ 
+b = 100
+c = 4
+print(b // c) 
+
+
+a = int(input("Enter a Amount: "))
+b= 4
+print (a // b)
