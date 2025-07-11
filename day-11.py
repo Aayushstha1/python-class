ebbc60e0549c3f1df51c7978f4df464cf07a9ca0
Aayushstha1 <sudipk339@gmail.com>
2025-07-11 00:59:57 -0700

    New

--- day-11.py
+++ day-11.py
@@ -40,3 +40,5 @@ print(a.disc())
 
 
 
+
+
---
+++ day-12.py
@@ -0,0 +1,33 @@
+# class animal:
+#     def __init__(self,name):
+#          self.name=name
+         
+         
+# class person (animal):
+#     def __init__(self,age, name):
+#         super().__init__(name)
+#         self.age=age
+        
+# p=person("Aayush",21)
+# print(p.age)
+# print(p.name)
+
+class father:
+    def __init__(self,name):
+        self.name=name
+        
+class mother:
+    def __init__(self,age):
+        self.age=age
+    
+class son(father,mother):
+    def __init__(self, name, age, Address):
+        father.__init__(self, name)
+        mother.__init__(self, age)
+        self.Address = Address
+s=son ("Aayush", 22,"Ktm")
+print(s.age)
+print(s.name)
+print(s.Address)
+        
+        
